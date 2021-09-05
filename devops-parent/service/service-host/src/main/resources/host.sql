be624890a9d486eeda4d610d70ad458ff220fd4b
I3OR2A <ioariroi@gmail.com>
2021-09-05 21:36:53 +0800

    Create host.sql

---
+++ devops-parent/service/service-host/src/main/resources/host.sql
@@ -0,0 +1,29 @@
+INSERT INTO `devops`.`tbl_host_info`
+(`TYPE_ID`,
+`HOSTNAME`,
+`IP_ADDRESS`,
+`PORT`,
+`REMARK`,
+`UPDATE_USER`,`UPDATE_TIME`,`CREATE_TIME`)
+VALUES
+(1,'TESTVM0001','192.168.56.12',80,'測試主機',1,sysdate(),sysdate());
+
+INSERT INTO `devops`.`tbl_host_info`
+(`TYPE_ID`,
+`HOSTNAME`,
+`IP_ADDRESS`,
+`PORT`,
+`REMARK`,
+`UPDATE_USER`,`UPDATE_TIME`,`CREATE_TIME`)
+VALUES
+(1,'TESTVM0002','192.168.56.13',80,'測試主機',1,sysdate(),sysdate());
+
+INSERT INTO `devops`.`tbl_host_info`
+(`TYPE_ID`,
+`HOSTNAME`,
+`IP_ADDRESS`,
+`PORT`,
+`REMARK`,
+`UPDATE_USER`,`UPDATE_TIME`,`CREATE_TIME`)
+VALUES
+(1,'TESTVM0003','192.168.56.14',80,'測試主機',1,sysdate(),sysdate());
